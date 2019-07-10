b1ca74d48fe02fc210a80ff61d6935851d87ba81
Daniel <ssejjembadan@gmail.com>
2019-07-10 18:24:25 +0300

    Added generic print any base method

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
-
 public class Factorial {
+    public static int factorial(int i)
+    {
+        /* Termination Condition */
+        if(i <= 1)
+            return 1;
+        /* Body, Recursive Expansion */
+        return i * factorial(i - 1);
+    }
+
+    //Print base 10 integers
+    public static void printInt1(int number)
+    {
+        char digit = (char) (number % 10 + '0');
+        number = number / 10;
+        if (number != 0)
+            printInt1(number/10);
+        System.out.print(" " + digit);
+    }
+
+    //print any base integers
+    public static void printInt2(int number, final int base)
+    {
+        String conversion = "0123456789ABCDEF";
+        char digit = (char) (number % base) ;
+        number = number / base;
+        if (number != 0)
+            printInt2(number,base);
+        System.out.print(" " + conversion.charAt(digit));
+    }
 }
