97b3ac911f78560c5cc2753c14b31524cfca2bd6
Daniel <ssejjembadan@gmail.com>
2019-07-10 18:43:34 +0300

    Added generic print any base method

--- src/GCD.java
+++ src/GCD.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+//Euclid’s algorithm is used to find gcd. GCD(n,m) == GCD(m, n mod m)
 
 public class GCD {
+
+    public static int gcd(int m, int n)
+    {
+        if(m<n)
+            return (gcd(n, m));
+        if(m%n == 0)
+            return (n);
+        return(gcd(n, m%n));
+    }
 }
--- src/TowerOfHanoi.java
+++ src/TowerOfHanoi.java
@@ -1,2 +1,25 @@
+/**
+ * The Tower of Hanoi (also called the Tower of Brahma) We are given three rods and N number
+ * of disks, initially all the disks are added to first rod (the leftmost one) in decreasing size order.
+ * The objective is to transfer the entire stack of disks from first tower to third tower (the rightmost
+ * one), moving only one disk at a time and never a larger one onto a smaller.
+ */
+
 public class TowerOfHanoi {
+
+    public static void towerOfHanoi(int num, char src, char dst, char temp)
+    {
+        if (num < 1)
+            return;
+        towerOfHanoi(num - 1, src, temp, dst);
+        System.out.println("Move"+num+"disk from peg"+src+" to peg " + dst);
+        towerOfHanoi(num - 1, temp, dst, src);
+    }
+
+    public static void main(String[] args)
+    {
+        int num = 4;
+        System.out.println("The sequence of moves in the Tower of Hanoi are : ");
+        towerOfHanoi(num, 'A', 'C', 'B');
+    }
 }
